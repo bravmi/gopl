722392f64146706c6d33fd0c10532c9308de2cc3
Michael T <bravmech@gmail.com>
2020-06-24 23:29:22 +0500

    init

---
+++ chap3/images/main.go
@@ -0,0 +1,231 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 61.
+//!+
+
+// Mandelbrot emits a PNG image of the Mandelbrot fractal.
+package main
+
+import (
+	"bufio"
+	"flag"
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"math/cmplx"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+	const (
+		width, height         = 720, 720
+		xmin, xmax    float64 = -2, +2
+		ymin, ymax    float64 = -2, +2
+		epsX                  = (xmax - xmin) / width
+		epsY                  = (ymax - ymin) / height
+		avg                   = false
+		zoom                  = 1.0
+	)
+	f := mandelbrot
+
+	file := flag.Bool("f", false, "write to 'images.png'")
+	flag.Parse()
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	if *file {
+		fi, err := os.Create("images.png")
+		defer fi.Close()
+		if err != nil {
+			log.Fatal("failed to create a file")
+		}
+
+		for py := 0; py < height; py++ {
+			y := float64(py)/height*(ymax-ymin) + ymin
+			for px := 0; px < width; px++ {
+				x := float64(px)/width*(xmax-xmin) + xmin
+				// Image point (px, py) represents complex value z.
+				z := complex(x, y)
+				img.Set(px, py, mandelbrot(z))
+			}
+		}
+
+		w := bufio.NewWriter(fi)
+		png.Encode(w, img) // NOTE: ignoring errors
+		w.Flush()
+	} else {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path != "/" {
+				http.NotFound(w, r)
+				return
+			}
+			w.Header().Set("Content-Type", "image/png")
+			// avg
+			avg := avg
+			if s := r.FormValue("avg"); s != "" {
+				b, err := strconv.ParseBool(s)
+				if err != nil {
+					log.Printf("invalid avg param: %v", err)
+				} else {
+					avg = b
+				}
+			}
+			// f
+			f := f
+			if s := r.FormValue("f"); s != "" {
+				switch s {
+				case "mandelbrot":
+					f = mandelbrot
+				case "newton":
+					f = newton
+				case "acos":
+					f = acos
+				case "sqrt":
+					f = sqrt
+				default:
+					f = mandelbrot
+				}
+			}
+			// xmin, xmax, ymin, ymax, zoom
+			xmin, xmax := xmin, xmax
+			ymin, ymax := ymin, ymax
+			zoom := zoom
+			fparams := []string{"xmin", "xmax", "ymin", "ymax", "zoom"}
+			for _, k := range fparams {
+				if s := r.FormValue(k); s != "" {
+					v, err := strconv.ParseFloat(s, 64)
+					if err != nil {
+						log.Printf("invalid %q param: %v", k, err)
+					} else {
+						switch k {
+						case "xmin":
+							xmin = v
+						case "xmax":
+							xmax = v
+						case "ymin":
+							ymin = v
+						case "ymax":
+							ymax = v
+						case "zoom":
+							zoom = v
+						}
+					}
+				}
+			}
+			lenX := xmax - xmin
+			lenY := ymax - ymin
+			epsX := lenX / width
+			epsY := lenY / height
+
+			midX := xmin + lenX/2
+			xmin = midX - lenX/2/zoom
+			xmax = midX + lenX/2/zoom
+			midY := ymin + lenY/2
+			ymin = midY - lenY/2/zoom
+			ymax = midY + lenY/2/zoom
+
+			for py := 0; py < height; py++ {
+				y := float64(py)/height*(ymax-ymin) + ymin
+				for px := 0; px < width; px++ {
+					x := float64(px)/width*(xmax-xmin) + xmin
+					// Image point (px, py) represents complex value z.
+					z := complex(x, y)
+					if avg {
+						colors := []color.Color{
+							f(complex(x-epsX, y-epsY)),
+							f(complex(x+epsX, y-epsY)),
+							f(complex(x-epsX, y+epsY)),
+							f(complex(x+epsX, y+epsY)),
+						}
+						img.Set(px, py, average(colors))
+					} else {
+						img.Set(px, py, f(z))
+					}
+				}
+			}
+
+			png.Encode(w, img)
+		})
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}
+}
+
+func average(colors []color.Color) color.Color {
+	var R, G, B, A uint8
+	n := len(colors)
+	for _, c := range colors {
+		r, g, b, a := c.RGBA()
+		R += uint8(r / uint32(n))
+		G += uint8(g / uint32(n))
+		B += uint8(b / uint32(n))
+		A += uint8(a / uint32(n))
+	}
+	return color.RGBA{R, G, B, A}
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.RGBA{0, 255 - contrast*n*2, 255 - contrast*n, 255}
+		}
+	}
+	return color.Black
+}
+
+//!-
+
+// Some other interesting functions:
+
+func acos(z complex128) color.Color {
+	v := cmplx.Acos(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{192, blue, red}
+}
+
+func sqrt(z complex128) color.Color {
+	v := cmplx.Sqrt(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{128, blue, red}
+}
+
+// f(x) = x^4 - 1
+//
+// z' = z - f(z)/f'(z)
+//    = z - (z^4 - 1) / (4 * z^3)
+//    = z - (z - 1/z^3) / 4
+func newton(z complex128) color.Color {
+	const iterations = 37
+	const contrast = 7
+	const eps = 1e-6
+	for i := uint8(0); i < iterations; i++ {
+		z -= (z - 1/(z*z*z)) / 4
+		if cmplx.Abs(z*z*z*z-1) < eps {
+			roots := []complex128{
+				complex(1, 0), complex(-1, 0), complex(0, 1), complex(0, -1),
+			}
+			switch {
+			case cmplx.Abs(z-roots[0]) < eps:
+				return color.RGBA{255 - contrast*i, 0, 0, 255}
+			case cmplx.Abs(z-roots[1]) < eps:
+				return color.RGBA{0, 255 - contrast*i, 0, 255}
+			case cmplx.Abs(z-roots[2]) < eps:
+				return color.RGBA{0, 0, 255 - contrast*i, 255}
+			case cmplx.Abs(z-roots[3]) < eps:
+				return color.RGBA{255 - contrast*i, 255 - contrast*i, 0, 255}
+			default:
+				return color.Gray{255 - contrast*i}
+			}
+		}
+	}
+	return color.Black
+}
---
+++ chap3/surface/main.go
@@ -0,0 +1,128 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 58.
+//!+
+
+// Surface computes an SVG rendering of a 3-D surface function.
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"net/http"
+	"os"
+)
+
+const (
+	width, height = 600, 320            // canvas size in pixels
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // pixels per x or y unit
+	zscale        = height * 0.4        // pixels per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+type zfunc func(x, y float64) float64
+
+func main() {
+	file := flag.Bool("f", false, "write to 'surface.svg'")
+	flag.Parse()
+
+	color := "grey"
+	if *file {
+		fi, err := os.Create("surface.svg")
+		defer fi.Close()
+		if err != nil {
+			log.Fatal("failed to create a file")
+		}
+		w := bufio.NewWriter(fi)
+		svg(w, f, color)
+		w.Flush()
+	} else {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "image/svg+xml")
+			if s := r.FormValue("color"); s != "" {
+				color = s
+			}
+			zf := f
+			if s := r.FormValue("zf"); s != "" {
+				switch s {
+				case "saddle":
+					zf = saddle
+				case "eggbox":
+					zf = eggbox
+				default:
+					zf = f
+				}
+			}
+			svg(w, zf, color)
+		})
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}
+}
+
+func svg(w io.Writer, f zfunc, color string) {
+	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			// for 3.3 you could return z values here as well
+			// and check how close they are to global min / max
+			ax, ay, err := corner(i+1, j, f)
+			bx, by, err := corner(i, j, f)
+			cx, cy, err := corner(i, j+1, f)
+			dx, dy, err := corner(i+1, j+1, f)
+			if err != nil {
+				fmt.Printf("skipping invalid polygon: %v\n", err)
+				continue
+			}
+			fmt.Fprintf(w, "<polygon style='stroke: %s' points='%g,%g %g,%g %g,%g %g,%g'/>\n", color,
+				ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	fmt.Fprintln(w, "</svg>")
+}
+
+func corner(i, j int, zf zfunc) (float64, float64, error) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// Compute surface height z.
+	z := zf(x, y)
+	if math.IsInf(z, 0) {
+		return 0, 0, fmt.Errorf("invalid height")
+	}
+
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy, nil
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y) // distance from (0,0)
+	return math.Sin(r) / r
+}
+
+func saddle(x, y float64) float64 {
+	a := 25.0
+	b := 17.0
+	a2 := a * a
+	b2 := b * b
+	return (y*y/a2 - x*x/b2)
+}
+
+func eggbox(x, y float64) float64 {
+	return 0.2 * (math.Cos(x) + math.Cos(y))
+}
+
+//!-
