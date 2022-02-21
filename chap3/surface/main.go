d8ec006e42f0713c1c7cb11882ddf48a94da6795
Michael T <bravmech@gmail.com>
2022-02-21 12:10:59 +0500

    add comment

--- chap3/surface/main.go
+++ chap3/surface/main.go
@@ -6,6 +6,7 @@
 
 // Surface computes an SVG rendering of a 3-D surface function.
 
+// covers all of ex1, ex2, ex3, ex4
 // usage: http://localhost:8080/?color=blue&zf=eggbox
 // usage: go run main.go -f
 package main
