f9cfcfc85abe8d929179e830c306b8b04d3dcbb6
Michael T <bravmech@gmail.com>
2022-05-17 11:55:03 +0400

    Add usage

--- chap3/ex13/main.go
+++ chap3/ex13/main.go
@@ -1,3 +1,4 @@
+// usage: go run main.go
 package main
 
 import (
@@ -17,14 +18,15 @@ func main() {
 		YiB // 1208925819614629174706176
 
 		KB = 1000
-		MB = 1000 * KB // 1e6
-		GB = 1000 * MB // 1e9
-		TB = 1000 * GB // 1e12
-		PB = 1000 * TB // 1e15
-		EB = 1000 * PB // 1e18
-		ZB = 1000 * EB // 1e21, overflows int (max ~9e18)
-		YB = 1000 * ZB // 1e24
+		MB = KB * KB // 1e6
+		GB = KB * MB // 1e9
+		TB = KB * GB // 1e12
+		PB = KB * TB // 1e15
+		EB = KB * PB // 1e18
+		ZB = KB * EB // 1e21, overflows int (max ~9e18)
+		YB = KB * ZB // 1e24
 	)
 
-	fmt.Println(EB)
+	fmt.Println("EiB:", EiB)
+	fmt.Println("EB:", EB)
 }
