e199faa93e0f673cb80c444dfe6552c1f200cddd
Michael T <bravmech@gmail.com>
2022-07-05 11:11:11 +0400

    Save ex 4.12

---
+++ chap4/ex12/main.go
@@ -0,0 +1,124 @@
+// Based on:
+// https://github.com/vinceyuan/gopl-solutions/blob/master/ch04/ex4.12/ex4.12.go
+//
+// usage:
+// go run main.go -from 1 -to 10
+// go run main.go -search sketch
+
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const indexFile = "index.json"
+const comicsUrlFormat = "https://xkcd.com/%d/info.0.json"
+
+type Index struct {
+	FilePath string
+	Comics   map[int]*Comic
+}
+
+type Comic struct {
+	Title      string
+	Transcript string
+	ImgUrl     string `json:"img"`
+}
+
+func getComic(url string) (*Comic, error) {
+	log.Println("fetching", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var comic Comic
+	if err := json.NewDecoder(resp.Body).Decode(&comic); err != nil {
+		return nil, err
+	}
+	return &comic, nil
+}
+
+func getIndex(filePath string) *Index {
+	index := &Index{
+		FilePath: filePath,
+		Comics:   make(map[int]*Comic),
+	}
+
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return index
+	}
+	if err := json.Unmarshal(data, &index.Comics); err != nil {
+		log.Fatal(err)
+	}
+	return index
+}
+
+func (i *Index) save() {
+	data, err := json.MarshalIndent(i.Comics, "", "\t")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := ioutil.WriteFile(i.FilePath, data, 0644); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (i *Index) build(fromNum, toNum int) {
+	for num := fromNum; num <= toNum; num++ {
+		if _, exists := i.Comics[num]; exists {
+			continue
+		}
+		url := fmt.Sprintf(comicsUrlFormat, num)
+		comic, err := getComic(url)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		i.Comics[num] = comic
+	}
+}
+
+func (i *Index) search(query string) []*Comic {
+	var foundComics []*Comic
+	query = strings.ToLower(query)
+	for _, comic := range i.Comics {
+		title := strings.ToLower(comic.Title)
+		transcript := strings.ToLower(comic.Transcript)
+		if strings.Contains(title, query) || strings.Contains(transcript, query) {
+			foundComics = append(foundComics, comic)
+		}
+	}
+	return foundComics
+}
+
+func main() {
+	fromNum := flag.Int("from", 0, "num from which to build index")
+	toNum := flag.Int("to", 1, "num to which to build index")
+	search := flag.String("search", "", "search for transcript and title")
+	flag.Parse()
+
+	index := getIndex(indexFile)
+	if *fromNum != 0 && *toNum != 0 {
+		index.build(*fromNum, *toNum)
+		index.save()
+	}
+	if *search != "" {
+		foundComics := index.search(*search)
+		for _, comic := range foundComics {
+			b, err := json.MarshalIndent(comic, "", "\t")
+			if err != nil {
+				continue
+			}
+			log.Println(string(b))
+		}
+	}
+}
