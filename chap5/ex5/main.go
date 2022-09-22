0738d315382c5ce130a99df8e940b6f4837e080d
Michael T <bravmech@gmail.com>
2022-09-22 13:04:03 +0400

    Save ex 5.5

---
+++ chap5/ex5/main.go
@@ -0,0 +1,57 @@
+// Usage:
+// go run main.go https://golang.org
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+// CountWordsAndImages does an HTTP GET request for the HTML
+// document url and returns the number of words and images in it.
+func CountWordsAndImages(url string) (words, images int, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		err = fmt.Errorf("parsing HTML: %s", err)
+		return
+	}
+	words, images = countWordsAndImages(doc)
+	return
+}
+
+func countWordsAndImages(n *html.Node) (words, images int) {
+	if n.Type == html.TextNode {
+		words += len(strings.Fields(n.Data))
+	}
+	if n.Type == html.ElementNode && n.Data == "img" {
+		images++
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		w, i := countWordsAndImages(c)
+		words += w
+		images += i
+	}
+	return
+}
+
+func main() {
+	for _, url := range os.Args[1:] {
+		words, images, err := CountWordsAndImages(url)
+		if err != nil {
+			fmt.Printf("words: %d, images: %d\n", words, images)
+			fmt.Fprintf(os.Stderr, "CountWordsAndImages: %v\n", err)
+			continue
+		}
+		fmt.Printf("words: %d, images: %d\n", words, images)
+	}
+}
