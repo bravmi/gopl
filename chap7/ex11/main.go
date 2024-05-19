9e0ed569f241367f30233ce56463b6792814ddbb
Michael T <bravmech@gmail.com>
2024-05-19 10:27:09 +0200

    Save ex. 7.11

---
+++ chap7/ex11/main.go
@@ -0,0 +1,115 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 195.
+
+// Http4 is an e-commerce server that registers the /list and /price
+// endpoint by calling http.HandleFunc.
+
+// usage:
+// go run main.go
+// http http://localhost:8000/list
+// http http://localhost:8000/price\?item\=socks
+// http http://localhost:8000/create\?item\=hat\&price\=10
+// http http://localhost:8000/update\?item\=hat\&price\=20
+// http http://localhost:8000/delete\?item\=hat
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+//!+main
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/create", db.create)
+	http.HandleFunc("/update", db.update)
+	http.HandleFunc("/delete", db.delete)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+//!-main
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+type database map[string]dollars
+
+var dbmu sync.Mutex
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	if price, ok := db[item]; ok {
+		fmt.Fprintf(w, "%s\n", price)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q\n", item)
+	}
+}
+
+func (db database) create(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+	if _, ok := db[item]; ok {
+		w.WriteHeader(http.StatusConflict)
+		fmt.Fprintf(w, "item already exists: %q\n", item)
+		return
+	}
+
+	f, err := strconv.ParseFloat(price, 32)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "invalid price: %q\n", price)
+		return
+	}
+	dbmu.Lock()
+	db[item] = dollars(float32(f))
+	dbmu.Unlock()
+	fmt.Fprintf(w, "item created: %q\n", item)
+}
+
+func (db database) update(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+	if _, ok := db[item]; !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+
+	f, err := strconv.ParseFloat(price, 32)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "invalid price: %q\n", price)
+		return
+	}
+	dbmu.Lock()
+	db[item] = dollars(float32(f))
+	dbmu.Unlock()
+	fmt.Fprintf(w, "item updated: %q\n", item)
+}
+
+func (db database) delete(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	if _, ok := db[item]; !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	delete(db, item)
+	fmt.Fprintf(w, "item deleted: %q\n", item)
+}
