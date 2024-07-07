b938fa9c14793120f23e8f45b1d5a8d3afa389f9
Michael T <bravmech@gmail.com>
2024-07-07 13:20:16 +0200

    Save ex. 8.1

---
+++ chap8/ex1/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"text/tabwriter"
+	"time"
+)
+
+type Host struct {
+	Name    string
+	Address string
+}
+
+func ParseHost(s string) Host {
+	parts := strings.Split(s, "=")
+	if len(parts) != 2 {
+		log.Fatalf("invalid host: %s", s)
+	}
+	h := Host{Name: parts[0], Address: parts[1]}
+	return h
+}
+
+func main() {
+	hosts := []Host{}
+	connections := map[string]net.Conn{}
+
+	for _, arg := range os.Args[1:] {
+		h := ParseHost(arg)
+		hosts = append(hosts, h)
+		conn, err := net.Dial("tcp", h.Address)
+		if err != nil {
+			log.Fatal(err)
+		}
+		connections[h.Name] = conn
+		defer conn.Close()
+	}
+
+	w := tabwriter.NewWriter(os.Stdout, 10, 0, 0, ' ', tabwriter.Debug)
+	for _, h := range hosts {
+		fmt.Fprintf(w, "%s\t", h.Name)
+	}
+	fmt.Fprintln(w)
+	w.Flush()
+
+	for {
+		for _, h := range hosts {
+			conn := connections[h.Name]
+			buf := make([]byte, 1024)
+			n, err := conn.Read(buf)
+			if err != nil {
+				log.Fatalf("error reading from %s: %v", h.Name, err)
+			}
+			fmt.Fprintf(w, "%s\t", strings.TrimSpace(string(buf[:n])))
+		}
+		fmt.Fprintln(w)
+		w.Flush()
+		time.Sleep(1 * time.Second)
+	}
+}
