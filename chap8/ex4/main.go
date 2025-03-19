0a8592b99b4906da8b638f3f9714c0b585c656d6
Michael T <bravmech@gmail.com>
2025-03-19 10:10:01 +0100

    Save ex. 8.4

---
+++ chap8/ex4/main.go
@@ -0,0 +1,67 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 223.
+
+// Reverb1 is a TCP server that simulates an echo.
+// usage:
+// go run chap8/ex4/main.go
+// go run chap8/ex3/main.go
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+// !+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	var wg sync.WaitGroup
+	for input.Scan() {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			echo(c, input.Text(), 1*time.Second)
+		}()
+	}
+	wg.Wait()
+	if tcpconn, ok := c.(*net.TCPConn); ok {
+		tcpconn.CloseWrite()
+		log.Println("closed write")
+	} else {
+		// NOTE: ignoring potential errors from input.Err()
+		c.Close()
+		log.Println("closed")
+	}
+}
+
+//!-
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn)
+	}
+}
