1306ec649a09d501aae5cab79ac557cf637fb706
Michael T <bravmech@gmail.com>
2026-05-09 10:09:54 +0400

    Remove redundant nolint directive in chap7/ex6

--- chap7/ex6/main.go
+++ chap7/ex6/main.go
@@ -41,7 +41,7 @@ type celsiusFlag struct{ Celsius }
 func (f *celsiusFlag) Set(s string) error {
 	var unit string
 	var value float64
-	fmt.Sscanf(s, "%f%s", &value, &unit) //nolint:errcheck
+	fmt.Sscanf(s, "%f%s", &value, &unit)
 	switch unit {
 	case "C", "°C":
 		f.Celsius = Celsius(value)
